a93cb83b896b551ef52c4ea138f653e7404686d6
scarfacejas <jas3putt@hotmail.com>
2016-09-21 22:23:58 +0100

    Our story first attempt

--- Hurst-SmithConsulting/Site/dev/HS.Consulting/HS.Consulting/Data/PartnerData.cs
+++ Hurst-SmithConsulting/Site/dev/HS.Consulting/HS.Consulting/Data/PartnerData.cs
@@ -1,6 +1,7 @@
 ﻿using HS.Consulting.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web;
 
@@ -16,8 +17,15 @@ namespace HS.Consulting.Data
             {
                 Company = "Helen Butler Coaching",
                 Url = "www.helenbutlercoaching.com",
-                Caption = "Your most valuable asset is your people, so it’s a no-brainer that you need every single one to be operating at their best and achieving their full potential.<br />I work with the whole spectrum of job roles within agencies because I believe passionately that coaching is not just for senior leadership roles, it can have a dramatic effect on bottom lines at all levels and in all functions, helping to foster a dynamic and collaborative working culture. "
+                Caption = "Your most valuable asset is your people, so it’s a no-brainer that you need every single one to be operating at their best and achieving their full potential.<br /><br />I work with the whole spectrum of job roles within agencies because I believe passionately that coaching is not just for senior leadership roles, it can have a dramatic effect on bottom lines at all levels and in all functions, helping to foster a dynamic and collaborative working culture. "
             };
+
+            _readOnlyPartners = new ReadOnlyCollection<Partner>(new[] { helenButler });
+        }
+
+        public static IReadOnlyList<Partner> Partners
+        {
+            get { return _readOnlyPartners; }
+        }
     }
-}
 }
